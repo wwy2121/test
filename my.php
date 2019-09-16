c5a191f82b7018127cb1c7459aa214d4b3ba7a72
吴问应 <wuwenying@theduapp.com>
2019-09-17 00:22:17 +0800

    111

---
+++ my.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wwy2121
+ * Date: 2019/9/17
+ * Time: 0:21
+ */
+
+namespace My\test;
+class my{
+    public function __construct(){
+        echo 'my_construct';
+    }
+}
